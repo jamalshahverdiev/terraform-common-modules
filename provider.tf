839538954781d1a02a2470ffb2ffde63dfc5355c
Jamal Shahverdiyev <jamal.shahverdiev@dif.tech>
2024-06-09 18:29:20 +0400

    Split variables

--- provider.tf
+++ provider.tf
@@ -1,34 +1,34 @@
 terraform {
   required_version = ">= 1.0.0"
-  required_providers {
-    vault = {
-      source  = "hashicorp/vault"
-      version = ">= 3.25.0, <= 4.2.0"
-    }
+  # required_providers {
+  #   vault = {
+  #     source  = "hashicorp/vault"
+  #     version = ">= 3.25.0, <= 4.2.0"
+  #   }
 
-    postgresql = {
-      source  = "cyrilgdn/postgresql"
-      version = "~> 1.22.0"
-    }
+  #   postgresql = {
+  #     source  = "cyrilgdn/postgresql"
+  #     version = "~> 1.22.0"
+  #   }
 
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.0"
-    }
+  #   random = {
+  #     source  = "hashicorp/random"
+  #     version = "~> 3.0"
+  #   }
 
-    mongodb = {
-      source  = "Kaginari/mongodb"
-      version = "0.1.7"
-    }
+  #   mongodb = {
+  #     source  = "Kaginari/mongodb"
+  #     version = "0.1.7"
+  #   }
 
-    keycloak = {
-      source  = "mrparkers/keycloak"
-      version = "4.4.0"
-    }
+  #   keycloak = {
+  #     source  = "mrparkers/keycloak"
+  #     version = "4.4.0"
+  #   }
 
-    ad = {
-      source  = "hashicorp/ad"
-      version = ">=0.4.4"
-    }
-  }
+  #   ad = {
+  #     source  = "hashicorp/ad"
+  #     version = ">=0.4.4"
+  #   }
+  # }
 }
