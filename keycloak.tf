cf4aa20ab6c440dec02a53f653ae65be4365f5d6
Jamal Shahverdiyev <jamal.shahverdiev@dif.tech>
2024-06-09 23:21:09 +0400

    Vault changes

--- keycloak.tf
+++ keycloak.tf
@@ -7,7 +7,7 @@ module "keycloak" {
   login_theme             = local.login_theme
   extra_config            = local.extra_config
   vault_mount_path        = local.vault_mount_path
-  vault_keycloak_key_path = "${local.account_id}/${local.region}/${local.env}/${local.path}/${local.services_secret_path}/${local.team}/${local.service_name}/${local.keycloak_path}"
+  vault_keycloak_key_path = "${local.account_id}/${local.env}/${local.region}/${local.path}/${local.services_secret_path}/${local.team}/${local.service_name}/${local.keycloak_path}"
   # Include only if keycloak_enabled is true
   # count = var.keycloak_enabled ? 1 : 0
 }
