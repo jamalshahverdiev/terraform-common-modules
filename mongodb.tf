b28cc0ab8ff495973dd18e854c7b11fed411847d
Jamal Shahverdiyev <jamal.shahverdiev@dif.tech>
2024-06-09 22:41:13 +0400

    Vault changes

--- mongodb.tf
+++ mongodb.tf
@@ -3,8 +3,8 @@ module "mongodb" {
   auth_database           = "${local.env}_${local.team}_${local.service_name}_db"
   user_name               = "${local.env}_${local.team}_${local.service_name}_user"
   vault_mount_path        = local.vault_mount_path
-  vault_mongodb_key_path  = "${local.account_id}/${local.region}/${local.env}/${local.path}/${local.services_secret_path}/${local.team}/${local.service_name}/${local.mongo_path}"
-  vault_mongodb_data_path = "${local.account_id}/${local.region}/${local.env}/${local.infra_path}/${local.mongo_path}/${local.secret}"
+  vault_mongodb_key_path  = "${local.account_id}/${local.env}/${local.region}/${local.path}/${local.services_secret_path}/${local.team}/${local.service_name}/${local.mongo_path}"
+  vault_mongodb_data_path = "${local.account_id}/${local.env}/${local.region}/${local.infra_path}/${local.mongo_path}/${local.secret}"
 
   # Include only if mongodb_enabled is true
   # count = var.mongodb_enabled ? 1 : 0
--- postgresql.tf
+++ postgresql.tf
@@ -3,8 +3,8 @@ module "postgresql" {
   db_name                    = "${local.env}_${local.team}_${local.service_name}_db"
   role_name                  = "${local.env}_${local.team}_${local.service_name}_role"
   vault_mount_path           = local.vault_mount_path
-  vault_postgresql_key_path  = "${local.account_id}/${local.region}/${local.env}/${local.path}/${local.services_secret_path}/${local.team}/${local.service_name}/${local.pg_path}"
-  vault_postgresql_data_path = "${local.account_id}/${local.region}/${local.env}/${local.infra_path}/${local.pg_path}/${local.secret}"
+  vault_postgresql_key_path  = "${local.account_id}/${local.env}/${local.region}/${local.path}/${local.services_secret_path}/${local.team}/${local.service_name}/${local.pg_path}"
+  vault_postgresql_data_path = "${local.account_id}/${local.env}/${local.region}/${local.infra_path}/${local.pg_path}/${local.secret}"
   # Include only if postgres_enabled is true
   # count = var.postgres_enabled ? 1 : 0
 }
