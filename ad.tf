477d3381b8baa7915d0744661ebe96915dc7ada3
Jamal Shahverdiyev <jamal.shahverdiev@dif.tech>
2024-06-30 11:16:57 +0400

    Add env variable to Keycloak and AD

--- ad.tf
+++ ad.tf
@@ -1,6 +1,6 @@
 module "ad" {
   source   = "./active-directory"
-  team     = "${local.team}-${local.service_name}"
+  team     = "${local.team}-${local.service_name}-${local.env}"
   scope    = local.scope
   category = local.category
   ad_ou_dn = local.ad_ou_dn
--- keycloak.tf
+++ keycloak.tf
@@ -1,8 +1,8 @@
 module "keycloak" {
   source                  = "./keycloak"
   realm_id                = local.realm_id
-  client_id               = "${local.team}-${local.service_name}"
-  client_name             = "${local.team}-${local.service_name}"
+  client_id               = "${local.team}-${local.service_name}-${local.env}"
+  client_name             = "${local.team}-${local.service_name}-${local.env}"
   callback_url            = local.callback_url
   login_theme             = local.login_theme
   extra_config            = local.extra_config
